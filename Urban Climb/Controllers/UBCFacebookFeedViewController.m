0f4fd3a0352802471e20263a2edaa071d4640d0c
Sean O'Connor <sean@sodev.co>
2019-10-14 14:18:31 +1100

    Adds progress view to facebook feed

--- Urban Climb/Controllers/UBCFacebookFeedViewController.m
+++ Urban Climb/Controllers/UBCFacebookFeedViewController.m
@@ -13,6 +13,7 @@
 @interface UBCFacebookFeedViewController ()
 
 @property (weak, nonatomic) IBOutlet WKWebView *webView;
+@property (weak, nonatomic) IBOutlet UIProgressView *progressView;
 
 @end
 
@@ -24,8 +25,35 @@
     
     NSURL *URL = [NSURL URLWithString:@"https://mobile.facebook.com/urbanclimbcollingwood/posts/"];
     NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:URL];
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:NULL];
     
     [self.webView loadRequest:urlRequest];
+}
+
+- (void)dealloc {
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
+
+    // if you have set either WKWebView delegate also set these to nil here
+    [self.webView setNavigationDelegate:nil];
+    [self.webView setUIDelegate:nil];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"estimatedProgress"] && object == self.webView) {
+        [self.progressView setAlpha:1.0f];
+        [self.progressView setProgress:self.webView.estimatedProgress animated:YES];
+
+        if(self.webView.estimatedProgress >= 1.0f) {
+            [UIView animateWithDuration:0.3 delay:0.3 options:UIViewAnimationOptionCurveEaseOut animations:^{
+                [self.progressView setAlpha:0.0f];
+            } completion:^(BOOL finished) {
+                [self.progressView setProgress:0.0f animated:NO];
+            }];
+        }
+    }
+    else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
 }
 
 @end
